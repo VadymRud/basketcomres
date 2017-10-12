87189f42f4892f5c397519978a1c84c1b408304c
rudvadim@gmail.com <Putin_huilo1>
2017-10-12 13:17:03 +0300

    use beautiful soup

--- basket/main.py
+++ basket/main.py
@@ -6,16 +6,22 @@ a = requests.get(' http://basket.com.ua')
 soup = BeautifulSoup(a.content, 'lxml')
 els_ul_new = soup.find_all('div', {'class': 'ul_new'})
 els_arr = []
+# for el in els_ul_new:
+#     a_href_el = html.fromstring(str(el))
+#     urls = a_href_el.xpath('//a/@href')
+#     els_arr.extend(urls)
+#BeautifulSoup all
 for el in els_ul_new:
-    a_href_el = html.fromstring(str(el))
-    urls = a_href_el.xpath('//a/@href')
-    els_arr.extend(urls)
+    a_href_el = BeautifulSoup(str(el), 'lxml').find_all('a')
+    for a_hef in a_href_el:
+        print(a_hef)
+        els_arr.extend(a_hef)
 
 ur = requests.get('http://basket.com.ua/news/64893.htm')
 soup_cont = BeautifulSoup(ur.content, 'lxml')
 
 soup_cont_zagolovok = soup_cont.find('div', {'class': 'tema'}).find('h1')
-
+soup_cont_date = soup_cont.find('div', {'class': 'tema'}).find('span').text
 
 
 #//*[@id="part1_1"]/div[2]/div[2]/ul/li[1]/a
